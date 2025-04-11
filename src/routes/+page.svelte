0b363369659c52fa128132a89dac22fd203597fd
james <jjgmckenzie@gmail.com>
2025-04-10 17:29:11 -0700

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,4 @@
+<div class="max-w-3xl mx-auto">
+    <h1 class="text-3xl">Welcome to the Main Page!</h1>
+    <a class="text-blue-800 underline" href="#/other">go to another page</a>
+</div>
---
+++ src/routes/other/+page.svelte
@@ -0,0 +1,4 @@
+<div class="max-w-3xl mx-auto">
+    <h1 class="text-3xl">This is the other page!</h1>
+    <a class="text-blue-800 underline" href="#/">return home</a>
+</div>
